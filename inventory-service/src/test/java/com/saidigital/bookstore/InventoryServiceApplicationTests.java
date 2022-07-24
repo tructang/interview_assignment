f968ddb19c8d6b7328fa4e1a3985116d9fc1fc33
Truc.Tang <trucgolden@gmail.com>
2022-07-24 17:25:06 +0700

    add inventory service

---
+++ inventory-service/src/test/java/com/saidigital/bookstore/InventoryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saidigital.bookstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
